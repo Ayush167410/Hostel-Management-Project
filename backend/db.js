895481c0b2a7c4ed79f8e4c47a0a29d07af751e8
Zahid8889 <zahidthelord@gmail.com>
2023-08-27 16:32:39 +0530

    feature update

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongoDB = async () => {
 
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/hostelmanagement');
+        await mongoose.connect('mongodb://127.0.0.1:27017/hostelmanagement1');
         const fetched_data = await mongoose.connection.db.collection('students');
         fetched_data.find({}).toArray().then((data)=>{
             console.log(data);
