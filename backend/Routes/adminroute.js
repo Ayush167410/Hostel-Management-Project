676ea3789f87f38a63c152775fd42bff0971e592
Zahid8889 <zahidthelord@gmail.com>
2023-08-27 22:39:51 +0530

    application process

--- backend/Routes/adminroute.js
+++ backend/Routes/adminroute.js
@@ -6,6 +6,8 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Studentapply = require("../models/studentapply");
+const StudentHostel = require("../models/studentHostel");
+const studentHostel = require("../models/studentHostel");
 const jwtsecret = "Thisisahostelmanagementproject";
 
 router.post(
@@ -45,87 +47,135 @@ router.post(
   }
 );
 router.post(
-    "/adminlogin",
-    [
-      body("email", "Invalid Email").isEmail(),
-      body("password", "Short Password").isLength({ min: 5 }),
-    ],
-    async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      let email = req.body.email;
-      try {
-        let adminData = await adminSchema.findOne({ email });
-        if (!adminData) {
-          return res
-            .status(400)
-            .json({ errors: "tr Logging with correct credential" });
-        }
-        const pwdCompare = await bcrypt.compare(
-          req.body.password,
-          adminData.password
-        );
-        if (!pwdCompare) {
-          return res
-            .status(400)
-            .json({
-              errors: "triy Logging with correct credential",
-              pass: adminData.password,
-              l: req.body.password,
-            });
-        }
-        const data = {
-          admin: {
-            id: adminData.id,
-          },
-        };
-        const authToken = jwt.sign(data, jwtsecret);
-        console.log(adminData);
-        // res.render(adminData);
-        return res.json({ success: true ,
-          authToken: authToken,
-          hostelno: adminData.hostelno,});
-      } catch (error) {
-        console.log(error);
-        // console.log("error happened");
-        res.json({ success: false });
-      }
+  "/adminlogin",
+  [
+    body("email", "Invalid Email").isEmail(),
+    body("password", "Short Password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  );
+    let email = req.body.email;
+    try {
+      let adminData = await adminSchema.findOne({ email });
+      if (!adminData) {
+        return res
+          .status(400)
+          .json({ errors: "tr Logging with correct credential" });
+      }
+      const pwdCompare = await bcrypt.compare(
+        req.body.password,
+        adminData.password
+      );
+      if (!pwdCompare) {
+        return res.status(400).json({
+          errors: "triy Logging with correct credential",
+          pass: adminData.password,
+          l: req.body.password,
+        });
+      }
+      const data = {
+        admin: {
+          id: adminData.id,
+        },
+      };
+      const authToken = jwt.sign(data, jwtsecret);
+      console.log(adminData);
+      // res.render(adminData);
+      return res.json({
+        success: true,
+        authToken: authToken,
+        hostelno: adminData.hostelno,
+      });
+    } catch (error) {
+      console.log(error);
+      // console.log("error happened");
+      res.json({ success: false });
+    }
+  }
+);
 
-  router.post(
-    "/adminpage",
-    async (req, res) => {
-      let email = req.body.email;
-      try {
-        let adminData = await adminSchema.findOne({ email }).select(
-          "-password"
-        );
-        if (!adminData) {
-          res.status(400).json({ errors: "Error displaying admin data" });
-        }
-        // console.log(adminData, "log from api");
-        let hostelStudents = await studentSchema.find({hostelno:adminData.hostelno});
-        res.send([adminData,hostelStudents]);
-      } catch (error) {
-        res.send("Server Error", error.message);
-      }
+router.post("/adminpage", async (req, res) => {
+  let email = req.body.email;
+  try {
+    let adminData = await adminSchema.findOne({ email }).select("-password");
+    if (!adminData) {
+      res.status(400).json({ errors: "Error displaying admin data" });
     }
-  );
-  router.post(
-    "/studentapplied",
-    async (req, res) => {
-      let hno = req.body.hostelno;
-      try {
-        
-        // console.log(adminData, "log from api");
-        let hostelStudents = await Studentapply.find({hostelno:hno});
-        res.send(hostelStudents);
-      } catch (error) {
-        res.send("Server Error", error.message);
-      }
+    // console.log(adminData, "log from api");
+    let hostelStudents = await studentHostel.find({
+      hostelno: adminData.hostelno,
+    });
+    res.send([adminData, hostelStudents]);
+  } catch (error) {
+    res.send("Server Error", error.message);
+  }
+});
+router.post("/studentapplied", async (req, res) => {
+  let hno = req.body.hostelno;
+  try {
+    // console.log(adminData, "log from api");
+    let hostelStudents = await Studentapply.find({ hostelno: hno });
+    res.send(hostelStudents);
+  } catch (error) {
+    res.send("Server Error", error.message);
+  }
+});
+router.post("/studentaccept", async (req, res) => {
+  let regno = req.body.regnumber;
+  try {
+    // console.log(adminData, "log from api");
+    let hostelStudent = await Studentapply.findOneAndRemove({
+      regnumber: regno,
+    });
+    try {
+      await StudentHostel.create({
+        name: hostelStudent.name,
+        email: hostelStudent.email,
+        phonumber: hostelStudent.phonumber,
+        regnumber: hostelStudent.regnumber,
+        rollnum: hostelStudent.rollnum,
+        dept: hostelStudent.dept,
+        fathername: hostelStudent.fathername,
+        mothername: hostelStudent.mothername,
+        gender: hostelStudent.gender,
+        roomno: hostelStudent.roomno,
+        semester: hostelStudent.semester,
+        dob: hostelStudent.dob,
+        hostelno: hostelStudent.hostelno,
+      });
+      res.json({ success: true });
+      console.log("Successfully accepted");
+    } catch (error) {
+      console.log(error);
+      // alert(error)
+      // console.log("error happened");
+      res.json({ success: false });
     }
-  );
+  } catch (error) {
+    res.send("Server Error", error.message);
+  }
+});
+router.post("/studentreject", async (req, res) => {
+  let regno = req.body.regnumber;
+  try {
+    let del = await Studentapply.findOneAndRemove({
+      regnumber: regno,
+    });
+    if(del){
+      console.log('successfully removed/api');
+      res.send({success:true});
+    }
+    else{
+      console.log('Already rejected');
+      res.send({success:false});
+
+    }
+
+  } catch (error) {
+    res.status(400).send("Server Error", error.message);
+  }
+});
 module.exports = router;
--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
-import Table from 'react-bootstrap/Table';
-import './tabl.css';
-
-
-
+import Table from "react-bootstrap/Table";
+import "./tabl.css";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AdminPage() {
-    const [adminData, setadminData] = useState({});
-    const [studentData, setstudentData] = useState([]);
-    const [studentapplied, setstudentapplied] = useState([]);
-  
+  let navigate = useNavigate();
+  const [adminData, setadminData] = useState({});
+  const [studentData, setstudentData] = useState([]);
+  const [studentapplied, setstudentapplied] = useState([]);
 
   const fetchAdmin = async () => {
     const email = localStorage.getItem("userEmail");
@@ -39,102 +37,126 @@ export default function AdminPage() {
     }).then(async (res) => {
       let response = await res.json();
       await setstudentapplied(response);
-      console.log('successf');
+      console.log("successf");
     });
   };
   useEffect(() => {
     fetchpplied();
   }, []);
-  console.log(adminData,'Admin');
-  console.log(studentData,'student');
-  console.log(studentapplied,'studenth');
+  console.log(adminData, "Admin");
+  console.log(studentData, "student");
+  console.log(studentapplied, "studenta");
 
-  const handleAccept=()=>
-  {
-    console.log('succ');
-
-  }
-  const handleReject=()=>
-  {
-
-  }
-
-  
+  const handleAccept = async (regno) => {
+    await fetch("http://localhost:5000/api/studentaccept", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ regnumber: regno }),
+    }).then(async (res) => {
+      let response = await res.json();
+      if (response.success) {
+        alert("success accepted");
+        navigate("/adminpage");
+      } else {
+        alert("Aceepting error try again");
+      }
+    });
+  };
+  const handleReject = async (regno) => {
+    await fetch("http://localhost:5000/api/studentreject", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ regnumber: regno }),
+    }).then(async (res) => {
+      let response = await res.json();
+      if (response.success) {
+        alert("success rejected");
+        navigate("/adminpage");
+      } else {
+        alert("Already rejected");
+      }
+    });
+  };
 
   return (
     <div className="tab">
       <Table striped bordered hover className="table-success">
-      <thead className="table-dark">
-        <tr>
-          <th>#</th>
-          <th>Credential</th>
-          <th>Detail</th>
-          
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Name</td>
-          <td>{adminData.name}</td>
-          
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Email</td>
-          <td>{adminData.email}</td>
-          
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Department</td>
-          <td>{adminData.dept}</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>Hostel No.</td>
-          <td>{adminData.hostelno}</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>Phone Number</td>
-          <td>{adminData.phonumber}</td>
-        </tr>
+        <thead className="table-dark">
+          <tr>
+            <th>#</th>
+            <th>Credential</th>
+            <th>Detail</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Name</td>
+            <td>{adminData.name}</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Email</td>
+            <td>{adminData.email}</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Department</td>
+            <td>{adminData.dept}</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Hostel No.</td>
+            <td>{adminData.hostelno}</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Phone Number</td>
+            <td>{adminData.phonumber}</td>
+          </tr>
         </tbody>
-        </Table>
-        <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Registration Number</th>
-          <th>Name</th>
-          <th>Roll no</th>
-          <th>Accept</th>
-          <th>Reject</th>
-          
-        </tr>
-      </thead>
-      <tbody>
-
-      {studentapplied.map(student=>
-        <tr>
-          <td>{student.regnumber}</td>
-          <td>{student.name}</td>
-          <td>{student.rollnum}</td>
-          <td><button className="btn btn-success btn-sm" onClick={handleAccept} >Accept</button></td>
-          <td><button className="btn btn-danger btn-sm" onClick={handleReject}>Reject</button></td>
-
-
-        </tr>)}
-
-     
-
-      </tbody>
       </Table>
-
-
-
-
-
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Registration Number</th>
+            <th>Name</th>
+            <th>Roll no</th>
+            <th>Accept</th>
+            <th>Reject</th>
+          </tr>
+        </thead>
+        <tbody>
+          {studentapplied.map((student) => (
+            <tr>
+              <td>{student.regnumber}</td>
+              <td>{student.name}</td>
+              <td>{student.rollnum}</td>
+              <td>
+                <button
+                  className="btn btn-success btn-sm"
+                  onClick={() => handleAccept(student.regnumber)}
+                >
+                  Accept
+                </button>
+              </td>
+              <td>
+                <button
+                  data-value="apply clicked"
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleReject(student.regnumber)}
+                >
+                  Reject
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
-  )
+  );
 }
